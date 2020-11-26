ca475d4b930b015cc1e9439e09fa586aff883fa0
yogaiw <agoy.indra@gmail.com>
2020-11-26 12:10:18 +0700

    Add Firebase Auth

--- Projects/app/src/main/java/com/stigma_mm1/boostravel/splashscreen.kt
+++ Projects/app/src/main/java/com/stigma_mm1/boostravel/splashscreen.kt
@@ -1,11 +1,22 @@
 package com.stigma_mm1.boostravel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatDelegate
 
 class splashscreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, welcome::class.java))
+            finish()
+        }, 3000)
     }
 }
--- Projects/app/src/main/java/com/stigma_mm1/boostravel/welcome.kt
+++ Projects/app/src/main/java/com/stigma_mm1/boostravel/welcome.kt
@@ -1,11 +1,24 @@
 package com.stigma_mm1.boostravel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class welcome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        btn_login.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+        })
+
+        btn_daftar.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, daftar::class.java)
+            startActivity(intent)
+        })
     }
 }
