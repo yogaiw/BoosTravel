40615a81313bdbb6c3b61f6bc0351f88ce8f423a
yogaiw <agoy.indra@gmail.com>
2020-11-30 14:42:01 +0700

    Menambahkan Sistem Login User
    Isu : Masih belum bisa membedakan akun mitra dengan akun user biasa

--- Projects/app/src/main/java/com/stigma_mm1/boostravel/login.kt
+++ Projects/app/src/main/java/com/stigma_mm1/boostravel/login.kt
@@ -1,17 +1,23 @@
 package com.stigma_mm1.boostravel
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_login.*
+
 
 class login : AppCompatActivity() {
 
     var valid: Boolean = true
+    val fAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    val fStore: FirebaseFirestore = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +26,17 @@ class login : AppCompatActivity() {
         btnLogin.setOnClickListener {
             checkField(edtEmail)
             checkField(edtPassword)
+
+            fAuth.signInWithEmailAndPassword(edtEmail.text.toString(), edtPassword.text.toString()).addOnSuccessListener(OnSuccessListener {
+                Toast.makeText(this, "Login Berhasil", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, home::class.java))
+            }).addOnFailureListener { OnFailureListener {
+                Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+            }}
         }
 
         daftarBtn.setOnClickListener {
             startActivity(Intent(this, daftar::class.java))
-
         }
     }
 
