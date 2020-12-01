f4dc1234b96ecdfc16461f762ce8065facccd68e
RizkyIsna <18102212@ittelkom-pwt.ac.id>
2020-12-01 12:34:59 +0700

    Melengkapi fungsi tombol pada activity home user

--- Projects/app/src/main/java/com/stigma_mm1/boostravel/home.kt
+++ Projects/app/src/main/java/com/stigma_mm1/boostravel/home.kt
@@ -10,6 +10,19 @@ class home : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        btnPengumuman1.setOnClickListener {
+            startActivity(Intent(this, DetailActivity::class.java))
+        }
+
+        btnPengumuman2.setOnClickListener {
+            startActivity(Intent(this, pengumuman::class.java))
+
+        }
+
+        btn_pesanan_anda.setOnClickListener {
+            startActivity(Intent(this, pesanan::class.java))
+        }
+
         btn_travel.setOnClickListener {
             startActivity(Intent(this, list_travel::class.java))
         }
